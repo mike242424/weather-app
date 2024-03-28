415ca010a16bb339273f39de638ca8f95eb92c3c
mike242424 <mikebyers24@gmail.com>
2024-03-28 11:32:07 -0400

    getting permissions and location coordinates

--- App.js
+++ App.js
@@ -3,8 +3,33 @@ import { style } from './App.style';
 import Home from './pages/Home/Home';
 import { ImageBackground } from 'react-native';
 import backgroundImg from './assets/background.png';
+import { useEffect, useState } from 'react';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
 
 const App = () => {
+  const [coordinates, setCoordinates] = useState();
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === 'granted') {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: '32.80', lng: '-80.00' });
+    }
+  }
+
+  console.log(coordinates);
   return (
     <ImageBackground
       imageStyle={style.img}
