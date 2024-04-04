8d2b06507328ec987711bce63b0ca32efaaf90f9
mike242424 <mikebyers24@gmail.com>
2024-04-04 12:23:54 -0400

    making txt component fontSize responsive

--- components/Txt/Txt.jsx
+++ components/Txt/Txt.jsx
@@ -1,9 +1,22 @@
-import { Text } from 'react-native';
+import { Text, useWindowDimensions } from 'react-native';
 import { style } from './Txt.style';
 
+const IPHONE_13_RATIO = 0.001072961373390558;
+
 const Txt = ({ children, anotherStyle, ...restProps }) => {
+  const fontSize = anotherStyle?.fontSize || style.text.fontSize;
+  const { height } = useWindowDimensions();
+  console.log(Math.round(fontSize * IPHONE_13_RATIO * height));
+
   return (
-    <Text style={[style.text, anotherStyle]} {...restProps}>
+    <Text
+      style={[
+        style.text,
+        anotherStyle,
+        { fontSize: Math.round(fontSize * IPHONE_13_RATIO * height) },
+      ]}
+      {...restProps}
+    >
       {children}
     </Text>
   );
